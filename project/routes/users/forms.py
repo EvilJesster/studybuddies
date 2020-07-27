223521df2eb13334bdb06b42ba1f783b4421fdd8
amandatong <amanda.g.tong@gmail.com>
2020-07-27 11:10:55 -0400

    amanda quick update

--- project/routes/users/forms.py
+++ project/routes/users/forms.py
@@ -12,7 +12,7 @@ class InfoForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
     studenttype = SelectField('High School or college?', choices=[('hs', 'High School'), ('C', 'College')],validators=[DataRequired()]) #TODO: look into having the skillist change based on this or smth
     contact = StringField('email', validators=[DataRequired(), Email()])
-    skillList = [('calc1', 'Calc 1'), ('calc2', 'Calc 2'), ('calc3', 'Calc 3'), ('chem1', 'Chem 1'), ('chem2', 'Chem 2') ] #TODO: talk to amanda about what to put here
+    skillList = [('calc', 'Calculus'), ('calc2', 'Calc 2'), ('calc3', 'Calc 3'), ('chem1', 'Chem 1'), ('chem2', 'Chem 2') ] #TODO: talk to amanda about what to put here
     strengths = SelectMultipleField('areas strong in', choices=skillList)
     weaknesses = SelectMultipleField('areas weak in', choices=skillList)
     pfp = StringField('url to your pfp', validators=[DataRequired(), URL() ])
