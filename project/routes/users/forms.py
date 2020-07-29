000e7db9f7ac79f1d7f6d5291a26866a756d0b80
amandatong <amanda.g.tong@gmail.com>
2020-07-29 08:04:38 -0400

    styled set up form and profile

--- project/routes/users/forms.py
+++ project/routes/users/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SelectField, SelectMultipleField, FormField, FieldList
 from wtforms.validators import DataRequired, Email, URL
 
+strengths_label = "I'm strong in..."
+weaknesses_label = "I need help in..."
 
 class UserForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
@@ -10,8 +12,8 @@ class UserForm(FlaskForm):
 
 class InfoForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
-    studenttype = SelectField('High School or college?', choices=[('HS', 'High School'), ('C', 'College')],validators=[DataRequired()]) #TODO: look into having the skillist change based on this or smth
     contact = StringField('email', validators=[DataRequired(), Email()])
+    studenttype = SelectField('High School or college?', choices=[('HS', 'High School'), ('C', 'College')],validators=[DataRequired()]) #TODO: look into having the skillist change based on this or smth
 
     # skillList = [('calc', 'Calculus'), ('calc2', 'Calc 2'), ('calc3', 'Calc 3'),
     #              ('chem1', 'Chem 1'), ('chem2', 'Chem 2') ] #TODO: talk to amanda about what to put here
@@ -24,41 +26,41 @@ class InfoForm(FlaskForm):
 class MathForm(FlaskForm):
     sublist = [('Calculus', 'Calculus'), ('Algebra', 'Algebra'),
                ('Statistics', 'Statistics'), ('Other math', 'Other math')]
-    strengths = SelectMultipleField('strengths', choices=sublist)
-    weaknesses = SelectMultipleField('weaknesses', choices=sublist)
+    strengths = SelectMultipleField(strengths_label, choices=sublist)
+    weaknesses = SelectMultipleField(weaknesses_label, choices=sublist)
 
 
 class BusinessForm(FlaskForm):
-    sublist = [('Comnmunications', 'Comnmunications'), ('Economics', 'Economics'),
+    sublist = [('Communications', 'Communications'), ('Economics', 'Economics'),
                ('Marketing', 'Marketing'), ('Management', 'Management')]
-    strengths = SelectMultipleField('strengths', choices=sublist)
-    weaknesses = SelectMultipleField('weaknesses', choices=sublist)
+    strengths = SelectMultipleField(strengths_label, choices=sublist)
+    weaknesses = SelectMultipleField(weaknesses_label, choices=sublist)
 
 
 class ScienceForm(FlaskForm):
     sublist = [('Chemistry', 'Chemistry'), ('Biology', 'Biology'), ('Physics', 'Physics'), ('Psychology', 'Psychology')]
-    strengths = SelectMultipleField('strengths', choices=sublist)
-    weaknesses = SelectMultipleField('weaknesses', choices=sublist)
+    strengths = SelectMultipleField(strengths_label, choices=sublist)
+    weaknesses = SelectMultipleField(weaknesses_label, choices=sublist)
 
 
 class EngineeringForm(FlaskForm):
     sublist = [('Architecture', 'Architecture'), ('Biomedical', 'Biomedical'), ('Chemical', 'Chemical'),
                ('Civil', 'Civil'), ('Electrical', 'Electrical'), ('Mechanical', 'Mechanical')]
-    strengths = SelectMultipleField('strengths', choices=sublist)
-    weaknesses = SelectMultipleField('weaknesses', choices=sublist)
+    strengths = SelectMultipleField(strengths_label, choices=sublist)
+    weaknesses = SelectMultipleField(weaknesses_label, choices=sublist)
 
 
 class HumanitiesForm(FlaskForm):
     sublist = [('Anthropology', 'Anthropology'), ('English', 'English'),('Gender Studies', 'Gender Studies'),
                ('Philosophy', 'Philosophy'), ('History', 'History'), ('Political Science', 'Political Science'),
                ('Foreign Languages', 'Foreign Languages'), ('Pre-law', 'Pre-law')]
-    strengths = SelectMultipleField('strengths', choices=sublist)
-    weaknesses = SelectMultipleField('weaknesses', choices=sublist)
+    strengths = SelectMultipleField(strengths_label, choices=sublist)
+    weaknesses = SelectMultipleField(weaknesses_label, choices=sublist)
 
 class ArtForm(FlaskForm):
     sublist = [('Performing', 'Performing'), ('Visual', 'Visual')]
-    strengths = SelectMultipleField('strengths', choices=sublist)
-    weaknesses = SelectMultipleField('weaknesses', choices=sublist)
+    strengths = SelectMultipleField(strengths_label, choices=sublist)
+    weaknesses = SelectMultipleField(weaknesses_label, choices=sublist)
 
 class SetupForm(FlaskForm):
     userinfo = FormField(InfoForm)
