115d7eb064040c3723b553350f8b3ce1060a7de2
EvilJesster <jhall00@stuy.edu>
2020-07-29 11:55:55 -0400

    changed mongopw so repo can go public, changed config file to fully untracked one

--- project/__init__.py
+++ project/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__, instance_relative_config=True)
 
 #app.config.from_object('Studybuddies-private') pycharm was misbehaving, may revert to this structure eventually
 app.debug = True
-app.config.from_pyfile('config.py')
+app.config.from_pyfile('newconfig.py')
 mongo = PyMongo(app)
 
 bcrypt = Bcrypt(app)
