dde928e9b55f4c62ba580f8134bb67892eb48dd5
EvilJesster <jhall00@stuy.edu>
2020-07-30 16:34:29 -0400

    minor changes

--- project/routes/users/helper.py
+++ project/routes/users/helper.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request,session, redirect, url_for
 from project import mongo
+
 users = mongo.db.users
 foll = mongo.db.followlist
 posts = mongo.db.posts
@@ -8,3 +9,5 @@ def isSetup(): #checks if a user has setup their account
     holder = users.find_one({'unique': session.get('unique')})
     if (holder['name'] == None):
         return (redirect(url_for('user.setup')))
+
+
