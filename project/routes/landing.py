8f678f3cc3538ba724023166505db9059b6b5bc4
EvilJesster <jhall00@stuy.edu>
2020-07-30 16:06:26 -0400

    feed ready for amanda

--- project/routes/landing.py
+++ project/routes/landing.py
@@ -2,17 +2,35 @@ from flask import Blueprint, render_template, session, redirect, url_for
 from project import mongo
 from datetime import datetime
 landing = Blueprint('landing', __name__)
-
+users = mongo.db.users
+foll = mongo.db.followlist
+posts = mongo.db.posts
 
 @landing.route('/')
 def tester(): #TODO:make this a real name
-    users = mongo.db.users
+    global users,foll, posts
     #userinfo.insert({'event': 'event_name', 'date': 'event_date', 'user': 'user_name'})
     if(session.get('lin') == True):
         holder = users.find_one({'unique': session.get('unique')})
         if(holder['name'] == None):
             return(redirect(url_for('user.setup')))
-        return(render_template('loggedin.html', user = holder['name'], time=datetime.now()))
+        ready = True
+        filt = {'unique': session.get('unique')}
+        folhold = []
+        if (foll.find_one(filt) != None):
+            folhold = foll.find_one({'unique': session.get('unique')})['followlist']
+        post = []
+        for i in folhold:
+            posthold = {}
+            if (posts.find_one({'username': i}) != None):
+                posthold['post'] = posts.find_one({'username': i})['plist'][::-1]
+            curuse = users.find_one({'username':i})
+            posthold['pfp'] = curuse['pfp']
+            posthold['username'] = curuse['username']
+            posthold['name'] = curuse['name']
+            post.append(posthold)
+
+        return(render_template('loggedin.html', user = holder['name'], ready=ready, post=post, time=datetime.now()))
     else:
         return(render_template('notloggedin.html', time = datetime.now()))
     return(render_template('base.html', time = datetime.now()))
